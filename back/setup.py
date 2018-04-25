9a90c8b2810fd8ec476f97abf9fc8b6a946a399e
Toni Mueller <dev@tonimueller.org>
2018-04-25 12:46:44 +0100

    have everything at the top level for now

--- back/setup.py
+++ back/setup.py
@@ -15,7 +15,7 @@ setup(
     author_email = 'dev@tonimueller.org',
     url = 'https://github.com/muellert/taiga-contrib-issue-colouring',
     license = 'AGPL',
-    package_dir={'': 'src'},
+    # package_dir={'': 'src'},
     include_package_data = True,
     packages = find_packages(),
     install_requires=[],
